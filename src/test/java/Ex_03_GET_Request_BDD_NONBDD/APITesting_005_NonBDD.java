bbc646a5240be688e44a6568b31d03be4cb7a325
HP <saurabhtyagi020@gmail.com>
2025-03-10 22:16:04 +0530

    10 March

--- src/test/java/Ex_03_GET_Request_BDD_NONBDD/APITesting_005_NonBDD.java
+++ src/test/java/Ex_03_GET_Request_BDD_NONBDD/APITesting_005_NonBDD.java
@@ -8,7 +8,7 @@ import org.testng.annotations.Test;
 
 public class APITesting_005_NonBDD {
 
-    RequestSpecification r;
+    RequestSpecification rs;
     Response response;
     ValidatableResponse vr;
 
@@ -16,11 +16,11 @@ public class APITesting_005_NonBDD {
     public void postiveTestCase()
     {
         String pincode = "110094";
-        r = RestAssured.given();
-        r.baseUri("https://api.zippopotam.us");
-        r.basePath("/IN/110094");
+        rs = RestAssured.given();
+        rs.baseUri("https://api.zippopotam.us");
+        rs.basePath("/IN/110094");
 
-        response = r.when().log().all().get();
+        response = rs.when().log().all().get();
 
         vr= response.then().log().all().statusCode(200);
     }
@@ -28,11 +28,11 @@ public class APITesting_005_NonBDD {
     @Test
     public void negativeTestCase()
     {
-        r=RestAssured.given();
-        r.baseUri("https://api.zippopotam.us");
-        r.basePath("/IN/-11");
+        rs=RestAssured.given();
+        rs.baseUri("https://api.zippopotam.us");
+        rs.basePath("/IN/-11");
 
-        response=r.when().log().all().get();
+        response=rs.when().log().all().get();
 
         vr= response.then().log().all().statusCode(404);
     }
