9c1888146e4b8bfb88f51ad3706bdae92dbdb7e2
HP <saurabhtyagi020@gmail.com>
2025-03-15 23:28:40 +0530

    15 March

--- src/test/java/Ex_09_TestngExample/APITesting_013_All_annotations.java
+++ src/test/java/Ex_09_TestngExample/APITesting_013_All_annotations.java
@@ -1,6 +1,64 @@
 package Ex_09_TestngExample;
 
+import org.testng.annotations.*;
+
 public class APITesting_013_All_annotations {
+
+    @BeforeSuite
+    public void test1()
+    {
+        System.out.println("I am before suite");
+    }
+
+    @BeforeTest
+    public void test2()
+    {
+        System.out.println("I am before test");
+    }
+    @BeforeClass
+    public void test3()
+    {
+        System.out.println("I am BeforeClass");
+    }
+
+    @BeforeMethod
+    public void test4()
+    {
+        System.out.println("I am BeforeMethod");
+    }
+
+    @Test
+    public void test5()
+    {
+        System.out.println("I am Test!");
+    }
+
+    @AfterMethod
+
+    public void test6()
+    {
+        System.out.println("I am After method");
+    }
+
+    @AfterClass
+
+    public void test7()
+    {
+        System.out.println("I am After class");
+    }
+
+    @AfterTest
+    public void test8()
+    {
+        System.out.println("I am After Test");
+    }
+
+    @AfterSuite
+    public void test9()
+    {
+        System.out.println("I am After Suite");
+    }
+
 
 
 
